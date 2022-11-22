f948d9892532c8469c49067e6cb33bb75e841174
Jakub Engielski <jakub5pl@gmail.com>
2022-11-23 00:07:00 +0100

    Regex is alive
    goutte to dump outer html
    regex to sort data

--- src/Scraper/Scraper.php
+++ src/Scraper/Scraper.php
@@ -14,9 +14,20 @@ class Scraper
         $selector='body';
         $output = $crawler->filter($selector);
         $html = $output->outerHtml();
-        var_dump($html);
 
-        dd($html);
+        $start = stripos($html, '<body>');
+
+        $end = stripos($html, '</body>', $offset = $start);
+
+        $length = $end - $start;
+
+        $htmlSection = substr($html, $start, $length);
+
+        preg_match_all('/<td class="nazwaDnia" colspan="4" style="font-size:13px">(?<dzien>.*?)<\/td>|<td class="godzina">(?<hours>.*?)<\/td><td class="test">(?<przedmiot>.*?)<br><\/td><td class="test">(?<wykladowca>.*?)<br><\/td><td class="test2">(?<sala>.*?)<br><\/td>/m', $htmlSection, $matches);
+
+        dd($matches);
+
+        dd($htmlSection);
 
 
 
