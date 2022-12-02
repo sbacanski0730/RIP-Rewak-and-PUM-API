ef141815ec02e7c444cfcd7949d84d4c110016e5
Jakub Engielski <jakub5pl@gmail.com>
2022-12-02 12:02:06 +0100

    array to json
    not filtered yet

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -20,10 +20,15 @@ class HomeController extends AbstractController
     public function fetch(): Response
     {
         $array = $this->scraper->scrap('http://www.plan.pwsz.legnica.edu.pl/checkSpecjalnosc.php?specjalnosc=s4PAM', '/<td class="nazwaDnia" colspan="4" style="font-size:13px">(?<dzien>.*?)<\/td>|<td class="godzina">(?<hours>.*?)<\/td><td class="test">(?<przedmiot>.*?)<br><\/td><td class="test">(?<wykladowca>.*?)<br><\/td><td class="test2">(?<sala>.*?)<br><\/td>/m');
-        dd($array);
+
+        for ($i = 0; $i <1; $i++) {
+        $removed = array_shift($array);
+        }
+
+        file_put_contents('test.json', json_encode($array));
 
 
-        return new Response('Scraped subjects');
+        return new Response('Json file generated sucesfully');
 
 
     }
