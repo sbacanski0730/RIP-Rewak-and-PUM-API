78896f98e55a3d1068f629c60352fa66e8f5a873
Jakub Engielski <jakub5pl@gmail.com>
2022-12-13 16:33:08 +0100

    fixing github weirdness

--- src/Controller/RoomController.php
+++ src/Controller/RoomController.php
@@ -17,22 +17,10 @@ class RoomController extends AbstractController
         $this->scraper = $scraper;
     }
 
-<<<<<<< HEAD
-    #[Route('/scrap/buildings', name: 'scrap/buildings')]
-=======
-    #[Route('/hello', name: 'hello')]
-    function hello() {
-        $http = 'http://www.plan.pwsz.legnica.edu.pl/schedule_view.php?site=show_sala.php&id=10';
-        $pattern = '/#">(?<budynek>.*?)</m';
-
-        $array_buildings = $this->scraper->buildingscrap($http, $pattern);
 
 
-        return new JsonResponse($array_buildings);
-    }
 
     #[Route('/scrap/rooms', name: 'scrap/rooms')]
->>>>>>> parent of 95e4130 (Revert "route and json names normalization")
 
     public function fetch(): Response
     {
